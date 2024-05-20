782e820d84a21b22d7697eed32568e01e227e704
Illia Bondar <kava.kav.23@gmail.com>
2024-05-20 12:02:16 +0200

    14.05.2024.dz done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive} from "@angular/router";
+import { AcerComponent } from './acer/acer.component';
+import { AppleComponent } from './apple/apple.component';
+import { AsusComponent } from './asus/asus.component';
+import { CanonComponent } from './canon/canon.component';
+import { DellComponent } from './dell/dell.component';
+import { HpComponent } from './hp/hp.component';
+import { LenovoComponent } from './lenovo/lenovo.component';
+import { LogitechComponent } from './logitech/logitech.component';
+import { SamsungComponent } from './samsung/samsung.component';
+import { TpLinkComponent } from './tp-link/tp-link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive,AcerComponent,AppleComponent,AsusComponent,CanonComponent,DellComponent,HpComponent,LenovoComponent,LogitechComponent,SamsungComponent,TpLinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AppleComponent } from './apple/apple.component';
+import { AsusComponent } from './asus/asus.component';
+import { CanonComponent } from './canon/canon.component';
+import { DellComponent } from './dell/dell.component';
+import { HpComponent } from './hp/hp.component';
+import { LenovoComponent } from './lenovo/lenovo.component';
+import { LogitechComponent } from './logitech/logitech.component';
+import { SamsungComponent } from './samsung/samsung.component';
+import { TpLinkComponent } from './tp-link/tp-link.component';
+import { AcerComponent } from './acer/acer.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+  { path:'',component:AppComponent},
+  { path: 'apple', component: AppleComponent },
+  { path: 'asus', component: AsusComponent },
+  { path: 'canon', component: CanonComponent },
+  { path: 'dell', component: DellComponent },
+  { path: 'hp', component: HpComponent },
+  { path: 'lenovo', component: LenovoComponent },
+  { path: 'logitech', component: LogitechComponent },
+  { path: 'samsung', component: SamsungComponent },
+  { path: 'tp-link', component: TpLinkComponent },
+  { path: 'acer', component: AcerComponent },
+];
